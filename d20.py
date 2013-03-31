95b0348df7eac67e66bdd9b170afdb7ac6d720e3
The Hungry Lumberjack <carsonwx@gmail.com>
2013-03-31 18:25:15 -0400

    added comments to d20

--- d20.py
+++ d20.py
@@ -1,5 +1,9 @@
 import random
+
 def gen_20(number_of_dice):
+	"""
+		returns the results of rolling <number_of_dice> d20 dice in a list	
+	"""
 	results = []
 	for i in range(1,(number_of_dice+1)):
 		results.append(int(random.uniform(1,21)//1))
