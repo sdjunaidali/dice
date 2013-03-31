e50813459c8bfa975ed9edfe2a8282cadec6b4c2
The Hungry Lumberjack <carsonwx@gmail.com>
2013-03-31 18:48:14 -0400

    added a basic structure to the interface

--- interface.py
+++ interface.py
@@ -1,2 +1,11 @@
-import d20
-import sys
+import d20, d6, sys
+
+while True:
+	type = raw_input("Please enter the type of die you want to roll\n(d6 or d20): ")
+	num = raw_input("Enter the number of die you want to roll: ")
+	print "type: ", type, "\nnumber: ", num
+	exit_case = raw_input("Quit? y/n: ")
+	if exit_case == "y":
+		break		
+	else:
+		pass
