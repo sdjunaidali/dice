2c841af6f5b86a165b3d7488222781a362cc8bfa
The Hungry Lumberjack <carsonwx@gmail.com>
2013-03-31 18:18:48 -0400

    fixed gen20 to handle multiple die

--- d20.py
+++ d20.py
@@ -1,3 +1,6 @@
 import random
-def gen_20():
-	return int(random.uniform(1,22)//1)
+def gen_20(number_of_dice):
+	results = []
+	for i in range(1,(number_of_dice+1)):
+		results.append(int(random.uniform(1,21)//1))
+	return results
