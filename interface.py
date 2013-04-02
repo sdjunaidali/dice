bf5ffd07281860e00a59438c35125657deb691c2
dynimao <dyni.mao@gmail.com>
2013-04-02 18:11:25 -0300

    Update to new code

--- interface.py
+++ interface.py
@@ -1,15 +1,18 @@
-import d20, d6, sys
+import d6
+import d20
+import sys
 
 while True:
-	type = raw_input("Please enter the type of die you want to roll\n(d6 or d20): ")
-	num = raw_input("Enter the number of die you want to roll: ")
-	if type == "d6":
-		result = SixSidedDice(num)
-	elif type =="d20":
-		result = roll20(num)
-	else:
-		print "Invalid input"
-	if(len(result) != 0):
-		print("The dices are: ")
-		for item in result:
-			print str(item)
+	try:
+		type = raw_input("Please enter the type of die you want to roll\n(d6 or d20): ")
+		num = raw_input("Enter the number of die you want to roll: ")
+		myResult = []
+		if type == "d6":
+			myResult = SixSidedDice(num)
+		elif type =="d20":
+			myResult = roll20(num)
+		else:
+			print "Invalid input"
+		print myResult
+	except ValueError:
+            print('Value error! Please try again!')
