acca75fa284cf92060ac5b13e698de21ebc545a4
Junaid <sd.junaidali@gmail.com>
2015-11-14 16:55:30 +0530

    Parentheses added in print statement of module, which were causing parentheses missing error in python 3.x

--- interface.py
+++ interface.py
@@ -3,18 +3,19 @@ import d20
 import sys
 
 while True:
-	try:
-		type = raw_input("Please enter the type of die you want to roll\n(d6 or d20): ")
-		num = int(raw_input("Enter the number of die you want to roll: "))
-		myResult = []
-		if type == "d6":
-			print "rolling d6s"
-			myResult = d6.SixSidedDice(num)
-		elif type =="d20":
-			print "rolling d20s"
-			myResult = d20.roll20(num)
-		else:
-			print "Invalid input"
-		print myResult
-	except ValueError:
-            print('Value error! Please try again!')
+        try:
+                type = input("Please enter the type of die you want to roll\n(d6 or d20): ")
+                num = int(input("Enter the number of die you want to roll: "))
+                myResult = []
+                if type == "d6":
+                        print ("rolling d6s")
+                        myResult = d6.SixSidedDice(num)
+                elif type =="d20":
+                        print ("rolling d20s")
+                        myResult = d20.roll20(num)
+                else:
+                        print ("Invalid input")
+
+                print (myResult)
+        except ValueError:
+                           print('Value error! Please try again!')
