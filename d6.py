58f377a664ceaf4adc51a889f6cb896db7685e44
Junaid <sd.junaidali@gmail.com>
2015-11-14 17:03:54 +0530

    Docstring added in function of module d6

--- d6.py
+++ d6.py
@@ -1,6 +1,10 @@
 import random
 
 def SixSidedDice(numberOfDice):
+	"""
+		returns the results of rolling <number_of_dice> d6 dice in a list	
+	"""
+	
 	myList = []
 	i = 0
 	while i < numberOfDice:
